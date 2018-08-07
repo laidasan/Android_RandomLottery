fbc808b2838bc0322624b0cfbda64807d0ebb47a
laidasan <tp6m4xup6tp6@gmail.com>
2018-08-08 01:25:47 +0800

    use new layout xml

--- app/src/main/java/com/dophin/randomlettery/LetteryActivity.java
+++ app/src/main/java/com/dophin/randomlettery/LetteryActivity.java
@@ -11,9 +11,9 @@ import java.util.*;
 
 public class LetteryActivity extends Activity {
     private List<String> datas = new ArrayList<>();
-    private boolean running;
-    private boolean wasrunning;
-    private int count = 0;
+    private boolean running;      //控制要不要跑random
+    private boolean wasrunning;   //紀錄是否正在跑Lottery
+    private int count = 0;        //紀錄執行續執行次數  -> 控制隨機跑的動畫次數
 
 
     @Override
@@ -28,6 +28,7 @@ public class LetteryActivity extends Activity {
 
     public void onClickAdd(View view) {
         EditText editText = (EditText)findViewById(R.id.addData);
+        //確認不再 run Lottery 與 加入的不是空字串的時候才可以加入樂透資料裡面
         if(!wasrunning && !editText.getText().toString().matches("")){
             datas.add(editText.getText().toString());
         }
@@ -35,6 +36,7 @@ public class LetteryActivity extends Activity {
     }
 
     public void onClickClear(View view) {
+        //不再 run Lottery的時候才能清除所有樂透資料
         if(!wasrunning) {
             datas.clear();
         }
@@ -43,6 +45,9 @@ public class LetteryActivity extends Activity {
 
     public void onClickLettery(View view) {
         TextView textView = (TextView)findViewById(R.id.DataView);
+        // 在start Lottery前先檢查datas裡是否有資料
+        //有的話才能開始lottery
+        //
         if(datas.iterator().hasNext()){
             count = 0;
             running = true;
@@ -64,8 +69,11 @@ public class LetteryActivity extends Activity {
         handler.post( new Runnable(){
             @Override
                     public void run() {
-
+                //利用亂數來達成樂透抽籤
                 int randomInt = (int) (Math.random() * datas.size());
+
+                //count 用來控制跑random動畫的次數(視覺上) 搭配者delayMillis使用
+                //利用設定Textview的內容來達成跑Lottery動畫
                 if (running && (count < 50)) {
                     count++;
                     textview.setText(datas.get(randomInt));
